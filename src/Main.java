b22243c706fc4693b7e2e0e1f53dae8799ae12e3
Max Meider <mmeider@mail.uni-mannheim.de>
2023-10-04 15:29:55 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+  public static void main(String[] args) {
+    State state = new State();
+    System.out.println(state.pits[1]);
+    System.out.println("Hello world!");
+  }
+}
---
+++ src/State.java
@@ -0,0 +1,11 @@
+public class State {
+  public int[] pits;
+  public int storeLeft;
+  public int storeRight;
+
+  public State(){
+    pits = new int[] {6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6};
+
+  }
+
+}
